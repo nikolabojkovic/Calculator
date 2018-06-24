88100375b3482c74a128edc484aa4924000bdce3
Nikola <nikolabojkovic@gmail.comm>
2018-06-24 18:02:37 +0200

    Initial commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,8 +13,79 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ResultLabel.text = "0"
     }
 
+    @IBOutlet weak var ResultLabel: UILabel!
+    var calculatedResult = 0.00
+    var currentNumber = 0.00
+    var operation = "none"
+    var resetLabel = false
+    
+    @IBAction func Number(_ sender: UIButton) {
+        if sender.titleLabel?.text == "0" && ResultLabel.text == "0" {
+            return
+        }
+        
+        if (sender.titleLabel?.text != "0" && ResultLabel.text == "0") {
+            ResultLabel.text = sender.titleLabel?.text
+            currentNumber = (NumberFormatter().number(from: (sender.titleLabel?.text)!)?.doubleValue)!
+            return
+        }
+        
+        if resetLabel {
+            if operation == "equal" {
+                calculatedResult = (NumberFormatter().number(from: (sender.titleLabel?.text)!)?.doubleValue)!
+            }
+            
+            ResultLabel.text = sender.titleLabel?.text
+            currentNumber = (NumberFormatter().number(from: (sender.titleLabel?.text)!)?.doubleValue)!
+            
+            resetLabel = false
+            return
+        }
+        
+        ResultLabel.text?.append((sender.titleLabel?.text)!)
+        currentNumber = (NumberFormatter().number(from: (ResultLabel.text)!)?.doubleValue)!
+    }
+    
+    @IBAction func Add(_ sender: UIButton) {
+        
+        operation = "add"
+        if currentNumber == 0 {
+            return
+        }
+        
+        resetLabel = true
+        calculatedResult += currentNumber
+        ResultLabel.text = "\(calculatedResult)"
+        currentNumber = 0.00
+    }
+    
+    @IBAction func Equals(_ sender: UIButton) {
+        
+        if operation == "none" {
+            return
+        }
+        
+        if operation == "add" {
+            calculatedResult += currentNumber
+            currentNumber = 0.00
+            operation = "none"
+        }
+        
+        resetLabel = true
+        ResultLabel.text = "\(calculatedResult)"
+        operation = "equal"
+    }
+    
+    @IBAction func Clear(_ sender: UIButton) {
+        ResultLabel.text = "0"
+        calculatedResult = 0
+        operation = "none"
+        resetLabel = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
